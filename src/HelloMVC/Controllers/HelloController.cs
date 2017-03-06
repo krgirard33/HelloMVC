d9a67f1cc12558a6b8969d9a9c1e8adb5f730700
krgirard33 <ken.girard@gmail.com>
2017-03-06 07:32:44 -0600

    initial commit

---
+++ src/HelloMVC/Controllers/HelloController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet me!' />" +
+                "</form>";
+
+            return Content(html, "text/html");
+        }
+        // /Hello
+        [Route("/Hello")]
+        [HttpPost]
+        public IActionResult Display(string name = "World")
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+
+        // Handle request to /Hello/NAME (URL segment)
+        [Route("/Hello/{name}")]
+        public IActionResult Index2(string name)
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye");
+        }
+
+        // /Monkey redirect which will totally ignore the string below, and instead call up 
+        // the Goodbye function. Pefectly stupid, but it shows how to redirect. 
+        [Route("/Monkey")]
+        [HttpGet]
+        public IActionResult Monkey(string name = "World")
+        {
+            string html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet me!' />" +
+                "</form>";
+
+            return Redirect("/Hello/Goodbye");
+        }
+    }
+}
